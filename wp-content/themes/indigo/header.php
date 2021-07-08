47f684ab57236fbef3494076a972705d4f03b052
Jamie Moore <jamiemoore2689@gmail.com>
2021-07-08 09:06:52 +0100

    Installed WP, added basic sass files and registered CPTs

---
+++ wp-content/themes/indigo/header.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<?php wp_head(); ?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&family=Syncopate:wght@400;700&display=swap" rel="stylesheet">
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentytwentyone' ); ?></a>
+
+	<?php get_template_part( 'template-parts/global/header' ); ?>
+
+	<div id="content" class="site-content">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
---
+++ wp-content/themes/indigo/template-parts/global/header.php
@@ -0,0 +1,15 @@
+<div class="site-header">
+    <div class="site-header__logo">
+        <h1>Indigo Cycles</h1>
+    </div>
+    <nav class="site-header__menu">
+        <button class="site-header__trigger"></button>
+        <ul>
+            <li><a href="#">Bikes</a></li>
+            <li><a href="#">Scooters</a></li>
+            <li><a href="#">Accessories</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Visit us</a></li>
+        </ul>
+    </nav>
+</div>
